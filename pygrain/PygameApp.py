41d717e546065a97d8f9eb504b77f3b8b1495668
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-12 12:29:30 +0100

    Class for main app

---
+++ pygrain/PygameApp.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+class PygameApp:
+    def __init__(self):
+        self.buttons = []
+
+    def check_events(self):
+        events = pygame.event.get()
+        for event in events:
+            pass
+
+    def mainloop(self):
+        pass

4cab236db8cb9ae4eb54d213828cf8360189db00
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-18 10:25:35 +0100

    Pass keyword arguments to super constructor.
    Use super draw method. Use get_x and get_y to get absolute position.

--- src/component/Button.py
+++ src/component/Button.py
@@ -4,20 +4,9 @@ from .util import show_text
 
 
 class Button(Component):
-    def __init__(self, parent, x=0, y=0, width=0, height=0, text=lambda: "",
-                 font_color=(0, 0, 0), bg_colour=(255, 255, 255),
-                 border_color=(0, 0, 0), border_thickness=1, font_size=10):
-        super().__init__(parent)
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+    def __init__(self, parent, x=0, y=0, text=lambda: '', **kwargs):
+        super().__init__(parent, x=x, y=y, **kwargs)
         self.text = text
-        self.font_colour = font_color
-        self.bg_colour = bg_colour
-        self.border_colour = border_color
-        self.border_thickness = border_thickness
-        self.font_size = font_size
         self.actions = {}
 
     def on_left_click(self, func):
@@ -33,21 +22,16 @@ class Button(Component):
         return self
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.bg_colour,
-                         (self.x, self.y, self.width, self.height))
-
-        pygame.draw.rect(screen, self.border_colour,
-                         (self.x, self.y, self.width, self.height),
-                         width=self.border_thickness)
-
-        show_text(screen, self.text(), self.x + self.width / 2, self.y + self.height / 2,
+        x, y = self.get_x(), self.get_y()
+        super().draw(screen)
+        show_text(screen, self.get_text(), x + self.width / 2, y + self.height / 2,
                   font_size=self.font_size)
 
     def mouseover(self):
         x, y = pygame.mouse.get_pos()
         return (
-                self.x <= x <= self.x + self.width and
-                self.y <= y <= self.y + self.height
+                self.get_x() <= x <= self.get_x() + self.width and
+                self.get_x() <= y <= self.get_x() + self.height
         )
 
     def event(self, name):
@@ -55,8 +39,7 @@ class Button(Component):
             return self.actions[name](self)
 
     def set_text(self, text):
-        self.text = text
-        self.parent.update()
+        self.set_property('text', text)
 
     def get_text(self):
-        return self.text()
+        return self.get_property('text')

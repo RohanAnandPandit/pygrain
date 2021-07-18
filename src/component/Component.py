4ad35b2fa5cb40a3122c1cb758eee7066cdb0924
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-18 11:13:07 +0100

    Methods to get and set actions Default implementation for mouseover and event methods. Add colour property.

--- src/component/Component.py
+++ src/component/Component.py
@@ -4,7 +4,8 @@ import pygame
 class Component:
     def __init__(self, parent, x=0, y=0, font_color=(0, 0, 0),
                  bg_colour=(255, 255, 255), border_color=(0, 0, 0),
-                 border_thickness=1, font_size=20, width=1, height=1):
+                 border_thickness=1, font_size=20, width=1, height=1,
+                 colour=(0, 0, 0)):
         self.parent = parent
         self.parent.add_component(self)
         self.x = x
@@ -18,6 +19,8 @@ class Component:
         self.border_colour = border_color
         self.border_thickness = border_thickness
         self.font_size = font_size
+        self.colour = colour
+        self.actions = {}
 
     def get_parent(self):
         return self.parent
@@ -32,7 +35,11 @@ class Component:
                          width=self.border_thickness)
 
     def event(self, name):
-        pass
+        if name in self.actions:
+            if 'click' in name and not self.mouseover():
+                return
+
+            return self.actions[name](self)
 
     def get_x(self):
         return self.get_parent().get_x() + self.x
@@ -54,3 +61,16 @@ class Component:
     def set_property(self, name, value):
         self.__setattr__(name, value)
         self.parent.update()
+
+    def get_action(self, name):
+        return self.actions[name]
+
+    def set_action(self, name, func):
+        self.actions[name] = func
+
+    def mouseover(self):
+        x, y = pygame.mouse.get_pos()
+        return (
+                self.get_x() <= x <= self.get_x() + self.width and
+                self.get_x() <= y <= self.get_x() + self.height
+        )

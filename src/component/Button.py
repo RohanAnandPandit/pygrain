06b8e6cfe6aaec1b7b527a4a042203c87f556229
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-18 11:14:32 +0100

    Use methods from superclass

--- src/component/Button.py
+++ src/component/Button.py
@@ -7,18 +7,17 @@ class Button(Component):
     def __init__(self, parent, x=0, y=0, text=lambda: '', **kwargs):
         super().__init__(parent, x=x, y=y, **kwargs)
         self.text = text
-        self.actions = {}
 
     def on_left_click(self, func):
-        self.actions['left click'] = func
+        self.set_action('left click', func)
         return self
 
     def on_right_click(self, func):
-        self.actions['right click'] = func
+        self.set_action('right click', func)
         return self
 
     def on_middle_click(self, func):
-        self.actions['middle click'] = func
+        self.set_action('middle click', func)
         return self
 
     def draw(self, screen):
@@ -26,17 +25,6 @@ class Button(Component):
         super().draw(screen)
         show_text(screen, self.get_text(), x + self.width / 2,
                   y + self.height / 2, font_size=self.font_size)
-
-    def mouseover(self):
-        x, y = pygame.mouse.get_pos()
-        return (
-                self.get_x() <= x <= self.get_x() + self.width and
-                self.get_x() <= y <= self.get_x() + self.height
-        )
-
-    def event(self, name):
-        if name in self.actions and self.mouseover():
-            return self.actions[name](self)
 
     def set_text(self, text):
         self.set_property('text', text)

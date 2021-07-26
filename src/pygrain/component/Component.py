dea8b3a91819862f003442ed7ba66acd11b14ba1
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-26 09:40:14 +0100

    Add feature to drag component after left click

--- src/pygrain/component/Component.py
+++ src/pygrain/component/Component.py
@@ -8,7 +8,7 @@ class Component:
     def __init__(self, parent, x=0, y=0, font_color=(0, 0, 0),
                  bg_colour=(255, 255, 255), border_color=(0, 0, 0),
                  border_thickness=1, font_size=20, width=1, height=1,
-                 colour=(0, 0, 0)):
+                 colour=(0, 0, 0), draggable=False):
         self.parent = parent
         self.parent.add_component(self)
         self.x = x
@@ -24,6 +24,10 @@ class Component:
         self.font_size = font_size
         self.colour = colour
         self.actions = {}
+        self.dragging = False
+        self.drag_offset_x, self.drag_offset_y = 0, 0
+        if draggable:
+            self.initialise_dragging()
 
     def get_parent(self):
         """
@@ -59,17 +63,23 @@ class Component:
 
         return True
 
-    def event(self, events):
+    def event(self, event):
         """
-        Call callback function for given combination of events (if it exists).
-        :param events: set of event names
+        Call callback function for a given binding that is a subset of the
+        current event.
+        :param event: set of event names
         :return: None
         """
-        if events in self.actions and self.valid_event(events):
-            for action in self.actions[events]:
-                action(self)
+        event = frozenset(event)
+        called = False
+        for curr in self.actions:
+            if curr.issubset(event) and self.valid_event(event):
+                for action in self.actions[curr]:
+                    action(self)
 
-            return True
+                called = True
+
+        return called
 
     def get_x(self):
         """
@@ -145,5 +155,43 @@ class Component:
         x, y = pygame.mouse.get_pos()
         return (
                 self.get_x() <= x <= self.get_x() + self.width and
-                self.get_x() <= y <= self.get_x() + self.height
+                self.get_y() <= y <= self.get_y() + self.height
         )
+
+    def set_dragging(self):
+        """
+        Calculate distances between current position of mouse and
+        top left (or centre) of the component.
+        :return:
+        """
+        self.dragging = True
+        x, y = pygame.mouse.get_pos()
+        self.drag_offset_x = x - self.get_x()
+        self.drag_offset_y = y - self.get_y()
+
+    def reset_dragging(self):
+        self.dragging = False
+
+    def drag_position(self):
+        """
+        If component is being dragged then the relative x, y coordinates
+        are set using the current position of the mouse and the offsets of
+        the mouse from the component when the user clicked on the component.
+        :return:
+        """
+        if not self.dragging:
+            return
+        x, y = pygame.mouse.get_pos()
+        self.x = x - self.get_parent().get_x() - self.drag_offset_x
+        self.y = y - self.get_parent().get_y() - self.drag_offset_y
+        self.parent.update()
+
+    def initialise_dragging(self):
+        """
+        Binds actions for dragging component.
+        :return:
+        """
+        self.bind({'left click'}, lambda target: self.set_dragging())
+        self.bind({'left up'}, lambda target: self.reset_dragging())
+        self.bind({'mousemotion'}, lambda target: self.drag_position())
+

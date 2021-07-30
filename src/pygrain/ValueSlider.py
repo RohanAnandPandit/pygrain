7f7adc57738452519c8cec0c9f56b66dbdaa5a78
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-30 16:20:29 +0100

    Add attributes for slider and draw method

--- src/pygrain/ValueSlider.py
+++ src/pygrain/ValueSlider.py
@@ -1,7 +1,28 @@
-from .Component import Component
-from .util import show_text
+from .Frame import Frame
+from .Point import Point
+import pygame
 
 
-class Slider(Component):
-    def __init__(self, parent, **kwargs):
+class ValueSlider(Frame):
+    def __init__(self, parent, start=0, end=10, increment=None, default=None,
+                 **kwargs):
         super().__init__(parent, **kwargs)
+        self.start = start
+        self.end = end
+        self.increment = increment
+        self.default = default
+
+        if self.default is None:
+            self.default = (self.start + self.end) / 2
+
+        self.point = Point(self, x=(self.width / 2), y=(self.height / 2),
+                           draggable=True, fixed_y=True, min_x=0, max_x=self.width)
+
+    def draw(self, screen):
+        super().draw(screen)
+        pygame.draw.line(screen, (0, 0, 0),
+                         (self.get_x(), self.get_y() + self.height / 2),
+                         (self.get_x() + self.width, self.get_y() + self.height / 2),
+                         2)
+        self.point.draw(screen)
+        self.update()

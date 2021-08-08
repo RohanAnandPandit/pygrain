9cb16bcd901402f14b01aa991576fa4f5e913b62
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-08-08 18:25:44 +0100

    Function to get width & height of text on screen

--- src/pygrain/util.py
+++ src/pygrain/util.py
@@ -20,6 +20,18 @@ def show_text(screen, text, x, y, font_colour=(0, 0, 0),
         pass
 
 
+def get_text_size(text, font_colour=(0, 0, 0), font_bg=(255, 255, 255),
+                  font_size=10):
+    # initialises font for displaying text
+    try:
+        for line in text.split('\n'):
+            basic_font = pygame.font.Font(FONT_PATH, font_size)
+            text = basic_font.render(text, True, font_colour, font_bg)
+            rect = text.get_rect()
+            return rect.width, rect.height
+    finally:
+        pass
+
 def show_image(screen, path, x, y):
     image = pygame.image.load(path).convert()
     screen.blit(image, (x, y))
